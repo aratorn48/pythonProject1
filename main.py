7dc93155d98d55bea31d1b6d96e33acc1edd816e
aratorn48 <aratorn48@mail.ru>
2024-06-30 00:28:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# 1st program
+print ( 9 ** 0.5 * 5)
+# 2st program
+print ( 9.99 > 9.98 and 1000 != 1000.1)
+# 3st program
+print ( (1234 % 1000 // 10 ) + (5678 % 1000 // 10 ))
+# 4st program
+print ((13.42 * 100 % 100 ) == ((int(42.13 ))))
+
