5ad5412c8f18b0d05f61ef71ac97fa58e596b3e6
aratorn48 <aratorn48@mail.ru>
2024-07-02 12:34:15 +0300

    Add files via upload

---
+++ task3 Переменные.py
@@ -0,0 +1,11 @@
+number_of_completed = 12
+number_of_hours_spent = 1.5
+course_name = 'Paython'
+time = 1.5 / 12 * 60
+time_per_task = time, 'minutes'
+print ('1 Количество выполненных ДЗ', number_of_completed)
+print ('2 Количество затраченных часов', number_of_hours_spent)
+print ('3 Название курса', course_name)
+print ('4 Время на одно задание', time_per_task)
+
+
